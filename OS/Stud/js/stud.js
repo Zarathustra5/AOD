0f55307883be5946fade19eb64a24e676a082a89
Comp <official1908teamfbi@gmail.com>
2022-01-30 14:49:11 +0300

    modified:   OS/Stud/js/stud.js

--- OS/Stud/js/stud.js
+++ OS/Stud/js/stud.js
@@ -1,35 +1,35 @@
 //Объявление класса Stud
 class Stud{
   constructor(surname){     //Конструктор
-    this.surname = surname;   //фамилия студента
-    this.score = [];          //оценки студента 
+    this._surname = surname;   //фамилия студента
+    this._score = [];          //оценки студента 
   }
   get getSurname(){         //Геттер, возвращающий фамилию
-    return this.surname;
+    return this._surname;
   }
   set setSurname(newSurname){   //Сеттер, изменяющий фамилию
     if (newSurname != null){
-      this.surname = newSurname;
+      this._surname = newSurname;
     }
   }
   getScore = function(num){     //Метод вывода оценки по индексу
-    if (num < this.score.length){
-      return this.score[num];
+    if (num < this._score.length){
+      return this._score[num];
     }else{
       return "Оценка по данному индексу не найдена";
     }
   }
   set addScore(newScore){       //Сеттер добавления оценки
     if (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5){
-      this.score.push(newScore);
+      this._score.push(newScore);
     }else{
       alert("Невозможная оценка");
     }
   }
   changeScore = function(newScore, num){    //Метод изменения оценки
     if (newScore == 2 || newScore == 3 || newScore == 4 || newScore == 5){
-      if (num < this.score.length){
-        this.score[num] = newScore;
+      if (num < this._score.length){
+        this._score[num] = newScore;
       }else{
         alert("Оценка по данному индексу не найдена");
       }
@@ -38,17 +38,17 @@ class Stud{
     }
   }
   get getScoreAverage(){          //Геттер, возвращающий среднее значение
-    if (this.score.length > 0){
+    if (this._score.length > 0){
       let sum = 0;
-      for (let el of this.score){
+      for (let el of this._score){
         sum += Number(el);
       }
-      return sum / this.score.length;
+      return sum / this._score.length;
     }else{
       return "Нет оценок";
     }
   }
   get getData(){   //Геттер, возвращающий полную информацию о студенте 
-    return `${this.surname}:  ${this.score}`
+    return `${this._surname}:  ${this._score}`
   }
 }
